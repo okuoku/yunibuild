9c78a5766241783486c4fa3f3cf4d9ddca19f083
okuoku <mjt@cltn.org>
2024-05-10 01:16:12 +0900

    Cygwin: Add tag and label

--- Docker-cygwin/build.cmake
+++ Docker-cygwin/build.cmake
@@ -18,4 +18,12 @@ if(rr)
     message(FATAL_ERROR "Err2")
 endif()
 
+execute_process(COMMAND
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+    docker build -f Dockerfile.ltsc2022 --tag ghcr.io/okuoku/yunibuild-cygwin-ltsc2022 .
+    RESULT_VARIABLE rr
+    )
 
+if(rr)
+    message(FATAL_ERROR "Err3")
+endif()
