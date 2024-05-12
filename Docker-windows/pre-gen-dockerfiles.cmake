91429a4b9bf38c360f4945b649e1790b6f8a4bd9
okuoku <mjt@cltn.org>
2024-05-12 13:53:28 +0900

    gha: Use absolute paths for Dockerfile generation

--- Docker-windows/pre-gen-dockerfiles.cmake
+++ Docker-windows/pre-gen-dockerfiles.cmake
@@ -1,9 +1,15 @@
-# Standard baseimage(LTSC2022 for GitHub Actions)
-set(BASEIMAGE mcr.microsoft.com/windows/servercore:ltsc2022)
-configure_file(Dockerfile-cygwin.in Dockerfile-cygwin.ltsc2022)
-configure_file(Dockerfile-msvc17.in Dockerfile-msvc17.ltsc2022)
-
-# On Windows10, 2004 is only suitable image for it
-set(BASEIMAGE mcr.microsoft.com/windows/servercore:2004)
-configure_file(Dockerfile-cygwin.in Dockerfile-cygwin.win10desktop)
-configure_file(Dockerfile-msvc17.in Dockerfile-msvc17.win10desktop)
+foreach(osversion ltsc2022 2004)
+    # ltsc2022: Standard baseimage(LTSC2022 for GitHub Actions)
+    # 2004: On Windows10, 2004 is only suitable image for it
+    set(BASEIMAGE mcr.microsoft.com/windows/servercore:${osversion})
+    if(${osversion} STREQUAL 2004)
+        set(label win10desktop)
+    else()
+        set(label ${osversion})
+    endif()
+    foreach(tool cygwin msvc17)
+        configure_file(${CMAKE_CURRENT_LIST_DIR}/Dockerfile-${tool}.in
+            ${CMAKE_CURRENT_LIST_DIR}/Dockerfile-${tool}.${label}
+            @ONLY)
+    endforeach()
+endforeach()
